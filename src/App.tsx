b60b9b2597d88534f28f7859283dd63d7537e84c
jburton321 <jburton.mail10@yahoo.com>
2026-01-21 12:56:07 -0500

    Updated tailwind.css

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ export default function App() {
   return (
     <div className="bg-white flex flex-col gap-0 justify-start overflow-hidden min-h-screen">
       <Navbar />
-      <div className="sm:hidden w-full">
+      <div className="sm:hidden w-full relative">
         <img
           src="/frame-4273216650.png"
           alt="Beach sunset"
           className="w-full h-auto object-cover aspect-video"
+        />
+        <img
+          src="/stamp.png"
+          alt="Future of Travel Focus Group"
+          className="absolute left-1/2 -translate-x-1/2 -bottom-12 w-24 h-24 z-10"
         />
       </div>
       <main className="w-full flex flex-col items-center flex-1">
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
 export const Hero = (): JSX.Element => {
   return (
-    <section className="w-full bg-gradient-to-r from-blue-900 to-blue-600 px-4 sm:px-6 py-4 sm:py-6">
+    <section className="w-full bg-gradient-to-r from-blue-900 to-blue-600 px-4 sm:px-6 pt-16 sm:pt-4 pb-4 sm:py-6">
       <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 items-center justify-start max-w-7xl mx-auto">
         <img
           src="/stamp.png"
           alt="Future of Travel Focus Group"
-          className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 shrink-0 object-contain"
+          className="hidden sm:block w-28 h-28 md:w-32 md:h-32 shrink-0 object-contain"
         />
         <div className="flex flex-col gap-1 sm:gap-2 items-center sm:items-start justify-start flex-1 text-center sm:text-left">
           <div
